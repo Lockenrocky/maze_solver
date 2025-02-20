1b0b02e6c35c3c64439763aeb6d35f6134b9a21a
Lockenrocky <steffenbengt@hotmail.com>
2025-02-21 00:36:52 +0100

    add a cell class to draw cell on the canvas

--- scenes/window.py
+++ scenes/window.py
@@ -39,3 +39,30 @@ class Line():
     
     def draw(self, canvas, fill_color="black"):
         canvas.create_line(self.x1, self.y1, self.x2, self.y2, fill=fill_color, width=2)
+
+class Cell():
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, y1, x2, y2):
+        if self.has_left_wall:
+            line = Line(Point(x1,y1), Point(x1,y2))
+            self._win.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(x1,y1), Point(x2,y1))
+            self._win.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(x2,y1), Point(x2,y2))
+            self._win.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(x1,y2), Point(x2,y2))
+            self._win.draw_line(line)
+
