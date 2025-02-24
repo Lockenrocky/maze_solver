7fc90bae1337b10e0579800ce749b7a0ec2437e4
Lockenrocky <steffenbengt@hotmail.com>
2025-02-24 23:25:29 +0100

    adding unittest and function to break down walls for the start and the end

--- scenes/window.py
+++ scenes/window.py
@@ -42,7 +42,7 @@ class Line():
         canvas.create_line(self.x1, self.y1, self.x2, self.y2, fill=fill_color, width=2)
 
 class Cell():
-    def __init__(self, win):
+    def __init__(self, win=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
@@ -69,9 +69,15 @@ class Cell():
         if self.has_top_wall:
             line = Line(Point(x1,y1), Point(x2,y1))
             self._win.draw_line(line)
+        if self.has_top_wall == False:
+            line = Line(Point(x1,y1), Point(x2,y1))
+            self._win.draw_line(line, fill_color="white")
         if self.has_bottom_wall:
             line = Line(Point(x1,y2), Point(x2,y2))
             self._win.draw_line(line)
+        if self.has_bottom_wall == False:
+            line = Line(Point(x1,y2), Point(x2,y2))
+            self._win.draw_line(line, fill_color="white")
 
     def draw_move(self, to_cell, undo=False):
         center1 = Point(abs(self._x1 + self._x2) // 2, abs(self._y1 + self._y2) // 2)
@@ -83,7 +89,7 @@ class Cell():
             self._win.draw_line(line, fill_color="red")
 
 class Maze():
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None):
         self._x1 = x1
         self._y1 = y1
         self._num_rows = num_rows
@@ -93,6 +99,7 @@ class Maze():
         self._win = win
         self._cells = []
         self._create_cells()
+        self._break_entrance_and_exit()
 
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -105,6 +112,7 @@ class Maze():
                 self._draw_cell(i, j)
 
 
+
     def _draw_cell(self, i ,j):
         if self._win is None:
             return
@@ -114,6 +122,12 @@ class Maze():
         y2 = y1 + self._cell_size_y
         self._cells[i][j].draw(x1, y1, x2, y2)
         self._animate()
+
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0,0)
+        self._cells[-1][-1].has_bottom_wall = False
+        self._draw_cell(self._num_cols-1, self._num_rows-1)
 
     def _animate(self):
         if self._win is None:
